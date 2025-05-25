2ad821abed5f5cb508697d6c8123b34108bc9d67
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-05-25 14:11:30 -0300

    Revert "Adicionando 1"
    This reverts commit 001ecaf452461345f97b3ffd50500e48a5ce11e4.

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ console.log("Prática 001 de Github");
 console.log("Código adicionado da branch console-feature");
 console.log("Código adicionado de um clone");
 alert("Código adicionado na alert-feature");
-alert("Adição 1");
